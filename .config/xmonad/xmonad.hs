30d7702f8d7fd582e70aefff3870e6414ac06b37
MitchMarq42 <dr.m.perseos@gmail.com>
2021-07-28 10:19:41 -0800

    add xmonad.hs, now in XDG compliant location.

---
+++ .config/xmonad/xmonad.hs
@@ -0,0 +1,55 @@
+-- Okay, now onto my actual config...
+import XMonad
+import XMonad.Config.Xfce
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import XMonad.Util.Cursor
+import XMonad.Util.EZConfig
+import XMonad.Actions.FloatKeys
+import XMonad.Layout.Spiral
+import XMonad.Layout.AvoidFloats
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.WindowSwallowing
+import Control.Monad (liftM2)
+
+myManageHook = composeAll
+        [ className =? "Brave-browser"          --> doShift "web"
+        , className =? "DesktopEditors"         --> viewShift "office"
+        , className =? "Xfce4-settings-manager" --> viewShift "settings"
+	    , className =? "Emacs"      			--> viewShift "office"
+        , className =? "Oblogout"	        	--> doFloat
+	    , className =? "st256color"		        --> doIgnore
+        --, className =? "mpv"			        --> doSwallow
+	    --, className =? "xfce4-panel"  		--> doIgnore
+	    , manageDocks
+        ]
+      where viewShift = doF . liftM2 (.) W.greedyView W.shift
+
+myWorkspaces = ["term","web","office","settings","5","6","7","8","9"]
+
+myTerminal = "alacritty"
+
+--myLayout = avoidStruts $ spiral (6/7)
+
+mySwallowHook = swallowEventHook ( className =? "Alacritty" ) ( return True )
+
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
+	[ ((modMask, xK_u), withFocused (keysMoveWindow (0, 10)) )
+	, ((modMask, xK_i), withFocused (keysMoveWindow (0, -10)) )
+	, ((modMask, xK_y), withFocused (keysMoveWindow (-10, 0)) )
+	, ((modMask, xK_o), withFocused (keysMoveWindow (10, 0)) )
+	, ((modMask, xK_b), sendMessage ToggleStruts)
+    , ((modMask, xK_Return), spawn myTerminal)
+	]
+
+main = xmonad xfceConfig
+       { modMask    = mod4Mask
+       , terminal   = myTerminal
+       , manageHook = myManageHook <+> manageHook xfceConfig
+       , workspaces = myWorkspaces
+       , keys       = myKeys <+> keys xfceConfig
+       --, layoutHook = myLayout
+       , handleEventHook = mySwallowHook <+> ewmhDesktopsEventHook <+> fullscreenEventHook <+> handleEventHook def
+       , startupHook = ewmhDesktopsStartup
+       }

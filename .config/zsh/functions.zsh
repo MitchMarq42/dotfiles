047f8d5aebbf894224604a61bf168201602a2c3d
MitchMarq42 <dr.m.perseos@gmail.com>
2021-11-03 21:17:47 -0800

    refactor tmux-clean, linting

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,36 +1,35 @@
 # Tmux clean function to kill detached sessions
 tmux-clean() {
-	for session in $(
-		tmux ls |
-		grep -v attached |
-		awk '{print $1}' |
-		sed 's/://'
-		)
-	do
-		tmux kill-session -t $session
-	done
+    sessions=$(tmux ls |
+        grep -v attached |
+        sed 's/\:\s.*$//'
+        )
+    for session in $sessions
+    do
+        tmux kill-session -t $session
+    done
 }
 [ -n "${TMUX}" ] && (
-	# Clear tmux history as well as terminal screen with "clear"
-	clear() {
-		/usr/bin/clear &&
-		tmux clear-history &&
-		/usr/bin/clear
-	}
+    # Clear tmux history as well as terminal screen with "clear"
+    clear() {
+        /usr/bin/clear &&
+        tmux clear-history &&
+        /usr/bin/clear
+    }
 )
 # Advanced pacman -Ql without nonsense.
 pacQl() {
-	/usr/bin/pacman -Ql $@ |
-	grep -v '/$' |
-	awk '{print $2}'
+    /usr/bin/pacman -Ql $@ |
+    grep -v '/$' |
+    awk '{print $2}'
 }
 lf () {
-	~/.local/bin/lfrun $@
+    ~/.local/bin/lfrun $@
 }
 
 # Do git bare repo stuff
 dots() {
-    /usr/bin/git --git-dir=$HOME/git/dotfiles --work-tree=$HOME $@
+    /usr/bin/git --git-dir=$HOME/.local/git/dotfiles --work-tree=$HOME $@
     ## I had a great explanation for all these things, but
     ## git does not take multi-line arguments.
 }
@@ -39,3 +38,5 @@ dots() {
 #sudo() {
 #    which doas >/dev/null && doas $@ ||
 #}
+
+# vim: set ft=sh

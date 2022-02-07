6aa72c2d78fa2df90e603eb4411489f7a68436f7
MitchMarq42 <dr.m.perseos@gmail.com>
2022-02-06 17:02:07 -0900

    clean up colors, add org faces

--- .config/emacs/mitch-theme.el
+++ .config/emacs/mitch-theme.el
@@ -8,26 +8,27 @@
   (
    ((class color) (min-colors #xFFFFFF)) ;; truecolor
    ((class color) (min-colors #xFF    )) ;; 256color
+   ((class color) (min-colors 16color )) ;; 16color
    )
 
   ;; Specify the color palette for each of the classes above.
-  (mitch-black   "#000000" "#000000")
-  (mitch-red     "#ff0000" "#ff0000")
-  (mitch-green   "#00aa00" "#00aa00")
-  (mitch-yellow  "#ffee11" "#ffee11")
-  (mitch-blue    "#0000ff" "#0000ff")
-  (mitch-magenta "#ff00ff" "#ff00ff")
-  (mitch-cyan    "#00ffff" "#00ffff")
-  (mitch-white   "#ffffff" "#ffffff")
-  (mitch-light-black   "#303030" "#303030")
-  (mitch-light-red     "#ffa000" "#ffa000")
-  (mitch-light-green   "#00c000" "#00c000")
-  (mitch-light-yellow  "#eeee99" "#eeee99")
-  (mitch-light-blue    "#0077ff" "#0077ff")
-  (mitch-light-magenta "#9f00aa" "#9f00aa")
-  (mitch-light-cyan    "#00c0c0" "#00c0c0")
-  (mitch-light-white   "#afafaf" "#afafaf")
-  (mitch-visual-bg     "#3f0090" "#3f0090")
+  (mitch-black   "#000000")
+  (mitch-red     "#ff0000")
+  (mitch-green   "#00aa00")
+  (mitch-yellow  "#ffee11")
+  (mitch-blue    "#0000ff")
+  (mitch-magenta "#ff00ff")
+  (mitch-cyan    "#00ffff")
+  (mitch-white   "#ffffff")
+  (mitch-light-black   "#303030")
+  (mitch-light-red     "#ffa000")
+  (mitch-light-green   "#00c000")
+  (mitch-light-yellow  "#eeee99")
+  (mitch-light-blue    "#0077ff")
+  (mitch-light-magenta "#9f00aa")
+  (mitch-light-cyan    "#00c0c0")
+  (mitch-light-white   "#afafaf")
+  (mitch-visual-bg     "#3f0090")
   )
 
  ;; specifications for Emacs faces.
@@ -42,13 +43,15 @@
   (font-lock-keyword-face (:foreground mitch-red :weight 'bold))
   (font-lock-constant-face (:foreground mitch-light-cyan))
   (font-lock-string-face (:foreground mitch-light-blue))
-  (font-lock-builtin-face (:foreground mitch-light-cyan :weight 'bold))
+  (font-lock-builtin-face (:foreground mitch-light-white :weight 'bold))
   (rainbow-delimiters-depth-1-face (:foreground mitch-light-magenta))
   (rainbow-delimiters-depth-2-face (:foreground mitch-magenta :weight 'normal))
   (rainbow-delimiters-depth-3-face (:foreground mitch-light-magenta))
   (rainbow-delimiters-depth-4-face (:foreground mitch-magenta :weight 'normal))
   (rainbow-delimiters-depth-5-face (:foreground mitch-light-magenta))
   (window-divider (:foreground mitch-light-magenta :weight 'bold))
+  (window-divider-first-pixel (:foreground mitch-light-magenta :weight 'bold))
+  (window-divider-last-pixel (:foreground mitch-light-magenta :weight 'bold))
   (yascroll:thumb-fringe (:foreground mitch-green :background mitch-green))
   )
 
@@ -70,21 +73,41 @@
 (menu-bar-mode -1)
 (set-fringe-mode 10)
 (setq window-divider-default-places t)
+(setq right-divider-width 5)
 (setq ring-bell-function 'ignore)
 ;; set font
 (defvar mitchcustom/default-font-size 130)
 (defvar mitchcustom/only-good-font "MesloLGS NF")
-(set-face-attribute
- 'default nil
- :font mitchcustom/only-good-font
- :height mitchcustom/default-font-size)
-(set-face-attribute
- 'fixed-pitch nil
- :font mitchcustom/only-good-font
- :height mitchcustom/default-font-size)
-(set-face-attribute
- 'variable-pitch nil
- :font "Ubuntu"
- :height mitchcustom/default-font-size)
+(set-face-attribute 'default nil
+                    :font mitchcustom/only-good-font
+                    :height mitchcustom/default-font-size)
+(set-face-attribute 'fixed-pitch nil
+                    :font mitchcustom/only-good-font
+                    :height mitchcustom/default-font-size)
+(set-face-attribute 'variable-pitch nil
+                    :font "Ubuntu"
+                    :height mitchcustom/default-font-size)
+
+;; Set faces for heading levels
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Ubuntu" :weight 'regular :height (cdr face)))
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
 
 (provide-theme 'mitch)

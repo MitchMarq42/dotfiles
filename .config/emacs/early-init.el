8d2d95529c2b0aebdae30f2aa96479945b5de69d
MitchMarq42 <dr.m.perseos@gmail.com>
2022-03-07 18:29:33 -0900

    emacs: startup function help

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,6 +1,7 @@
-;; Tell the emacs startup time instead of the banal
-;; "For information about GNU Emacs and the GNU system, type C-h C-a."
 (defun display-startup-echo-area-message ()
+  "A re-definition of the function.
+Tell the emacs startup time instead of the banal
+\"For information about GNU Emacs and the GNU system, type C-h C-a.\""
   (message (emacs-init-time)))
 
 ;; Crash the computer by overloading memory
@@ -12,10 +13,10 @@
 ;; Hide default dashboard
 (setq inhibit-startup-messages t)
 (setq inhibit-startup-screen t)
-;; diable stupid file open box thingy
-(setq use-file-dialog nil)
-(setq use-dialog-box nil)
 
 ;; Don't pop up error window on native-comp emacs
 (setq native-comp-async-report-warnings-errors 'silent)
 
+;; Run stuff after opening a new frame
+(setq server-after-make-frame-hook (mitch/graphical-setup))
+(setq before-make-frame-hook (mitch/graphical-setup))

f02c1fcf8875c1cd834a6fe86f5e6081707c6d6f
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-10 11:15:13 -0800

    add user.js for firefox

---
+++ .config/mozilla/firefox/mitch.default/user.js
@@ -0,0 +1,17 @@
+// start at brave-search
+user_pref("browser.startup.homepage", "https://search.brave.com/");
+user_pref("browser.urlbar.placeholderName", "Brave");
+// Don't show bookmark icons
+user_pref("browser.chrome.site_icons", false);
+user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
+user_pref("experiments.supported", false);
+user_pref("experiments.activeExperiment", false);
+user_pref("experiments.activeExperiment", false);
+user_pref("network.allow-experiments", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("dom.flyweb.enabled", false);
+user_pref("extensions.shield-recipe-client.enabled", false);
+user_pref("extensions.shield-recipe-client.api_url", "");
+
+user_pref("extensions.webextensions.uuids", {"formautofill@mozilla.org":"2cbff06a-3086-4977-8ebb-5d91f8f6213f", "pictureinpicture@mozilla.org":"1794f2ce-bf57-46f3-a2f1-e6a32458b4c5", "screenshots@mozilla.org":"bccd10f0-b9d2-4008-8af2-91ffd98f425f", "webcompat-reporter@mozilla.org":"fbc7ac8b-1145-49bb-af22-89ae9b5d235f", "webcompat@mozilla.org":"b4e46991-5b78-4880-ba3f-b789aa6b8f1f", "default-theme@mozilla.org":"3411fed2-ae95-4b8a-b10f-d3141c615c7b", "addons-search-detection@mozilla.com":"5c507124-83f8-47f2-b6d7-3d2350115c83", "ddg@search.mozilla.org":"866703d4-7364-4ac8-b17a-68ae89de0264", "vimium-c@gdh1995.cn":"f07987a1-50b1-4073-8695-ef326cc76aa4", "firefox-compact-dark@mozilla.org":"dbac04e1-3410-45f4-8ffb-a27dfa9f3ff3", "{99c277af-d778-4a0b-9faa-b1d8165f0a55}":"18c54ecc-507d-4f5b-a5f4-07ccf74b95b3"})

3e4e863df44ae68e0fd029df73652f5e4193af47
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-10 17:58:11 -0800

    emacs: eshell: broken fake terminal simulator

--- .config/emacs/lisp/eshell-settings.el
+++ .config/emacs/lisp/eshell-settings.el
@@ -8,6 +8,14 @@ your emacs is but an oyster..."
   "Clear the scrollback buffer, like `clear' in
 a real shell"
   (eshell/clear-scrollback))
-
+(defun eshell/faketty (args)
+  "USAGE: `faketty ARGS` where ARGS is anything that spews colors.
+Credit: https://stackoverflow.com/questions/1401002/how-to-trick-an-application-into-thinking-its-stdout-is-a-terminal-not-a-pipe"
+  (let
+      ((shell-command-dont-erase-buffer t))
+    (shell-command
+     (concat "script -qfc " args " /dev/null")
+     (current-buffer))
+    ))
 
 (provide 'eshell-settings)

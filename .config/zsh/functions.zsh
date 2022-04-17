03e2c540dad09071af01fe29b65246bc3085a7f3
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-17 07:10:12 -0800

    zsh: functions.zsh now part of tangling README

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,40 +1,21 @@
-# Tmux clean function to kill detached sessions
 tmux-clean() {
     sessions=$(tmux ls |
-        grep -v attached |
-        sed 's/\:\s.*$//'
-        )
+                   grep -v attached |
+                   sed 's/\:\s.*$//'
+            )
     for session in $sessions
     do
         tmux kill-session -t $session
     done
 }
+
 [ -n "${TMUX}" ] && (
-    # Clear tmux history as well as terminal screen with "clear"
+    clear=$(which clear)
     clear() {
-        /usr/bin/clear &&
-        tmux clear-history &&
-        /usr/bin/clear
+        $clear &&
+            tmux clear-history &&
+            $clear
     }
 )
-# Advanced pacman -Ql without nonsense.
-pacQl() {
-    /usr/bin/pacman -Ql $@ |
-    grep -v '/$' |
-    awk '{print $2}'
-}
-lf () {
-    ~/.local/bin/lfrun $@
-}
-
-# Do git bare repo stuff (moved to separate file)
-# dots() {
-#     $HOME/.local/bin/dots $@
-# }
-
-# Sudo is a bitch so bitch about it
-#sudo() {
-#    which doas >/dev/null && doas $@ ||
-#}
 
 # vim: set ft=sh :

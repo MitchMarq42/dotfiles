e94ba604f6fbe3e84ce943ed08acba8477ca5f97
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-17 11:29:35 -0800

    setup script: add logic and context

--- .local/bin/mitch-setup.sh
+++ .local/bin/mitch-setup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Get the dots function itself
-curl https://raw.githubusercontent.com/MitchMarq42/dotfiles/main/.local/bin/dots > ~/.local/bin/dots
+[ -f ~/.local/bin/dots ] ||
+    curl https://raw.githubusercontent.com/MitchMarq42/dotfiles/main/.local/bin/dots > ~/.local/bin/dots
 
-git clone https://github.com/mitchmarq42/dotfiles --bare ~/.local/git/dotfiles
+[ -d ~/.local/git/dotfiles ] ||
+    git clone https://github.com/mitchmarq42/dotfiles --bare ~/.local/git/dotfiles
 
 # run
-dots checkout -f
+~/.local/bin/dots checkout -f
 
-curl 'https://i.ytimg.com/vi/0NHIlU4l02E/maxresdefault.jpg' > .local/share/backgrounds/shockwave.jpg
-
-gnomes=$(ls ~/.config/gnome-custom-configs/ |
-	     grep gsets |
-      sed 's/\.gsets$//g')
+gnomes=$(
+    ls ~/.config/gnome-custom-configs/ |
+	grep gsets |
+	sed 's/\.gsets$//g'
+      )
 
 for prop in $gnomes
 do
@@ -23,15 +25,6 @@ dots submodule init
 dots submodule update
 
 sudo dnf upgrade -y
-sudo dnf search chsh
-sudo dnf install util-linux
-sudo dnf install zsh emacs qutebrowser gnome-tweak-tool gnome-extensions
-sudo dnf install zsh emacs qutebrowser gnome-tweak-tool gnome-shell-extensions
-sudo dnf install zsh emacs qutebrowser gnome-tweak-tool neovim
-sudo dnf search gnome extensions
-sudo dnf install gnome-extensions-app
-sudo dnf install gnome-extensions-app util-linux-user
-sudo dnf install gnome-extensions-app 
-sudo dnf install util-linux-user
+sudo dnf install util-linux zsh emacs qutebrowser gnome-tweak-tool neovim gnome-extensions-app util-linux-user
 
 chsh $(which zsh)

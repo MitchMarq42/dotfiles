091350c08239c04b91cf0f16a6df9d09f6f069f2
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-26 18:26:05 -0800

    setup.sh: add ubuntu installation

--- .local/bin/mitch-setup.sh
+++ .local/bin/mitch-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# # On Ubuntu do:
+# sudo apt install git curl emacs zsh
+
 # Get the dots function itself
 [ -f ~/.local/bin/dots ] ||
-    curl https://raw.githubusercontent.com/MitchMarq42/dotfiles/main/.local/bin/dots > ~/.local/bin/dots
+    curl https://raw.githubusercontent.com/MitchMarq42/dotfiles/main/.local/bin/dots > ~/.local/bin/dots && chmod +x ~/.local/bin/dots
 
 [ -d ~/.local/git/dotfiles ] ||
     git clone https://github.com/mitchmarq42/dotfiles --bare ~/.local/git/dotfiles

872ffd2c86a34850d032900aa00378259e371adb
MitchMarq42 <dr.m.perseos@gmail.com>
2022-04-27 07:25:39 -0800

    make brave work on fedora

--- .local/bin/mitch-setup.sh
+++ .local/bin/mitch-setup.sh
@@ -33,6 +33,14 @@ dots submodule update
 # # On fedora do:
 # sudo dnf upgrade -y
 # sudo dnf install util-linux zsh emacs qutebrowser gnome-tweak-tool neovim gnome-extensions-app util-linux-user
+# sudo dnf install dnf-plugins-core
+
+# sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+
+# sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+# sudo dnf install brave-browser
+
 
 # # On macos do:
 # # check out

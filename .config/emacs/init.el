536cd782607e13dd0c3393b08d5c22d646f006c4
MitchMarq42 <dr.m.perseos@gmail.com>
2022-05-03 20:41:31 -0800

    emacs: lint init-file

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,4 +1,5 @@
-;; Mitch's...
+;;; init.el --- basic initial declarations
+;;; Commentary:
 ;;  _         _  _            _
 ;; (_) _ __  (_)| |_     ___ | |
 ;; | || '_ \ | || __|   / _ \| |
@@ -7,12 +8,13 @@
 ;;
 ;; '((above text graphic generated with command `figlet -k "init.el"'))
 
+;;; Code:
 ;; Speed up loading/finding files
 (let
     ((file-name-handler-alist nil))
 
   ;; Load the files that I put my settings in...
-  (setq mitch-directory
+  (defvar mitch-directory
 	(directory-file-name
 	 (concat user-emacs-directory
 		 (convert-standard-filename "lisp/"))))
@@ -26,11 +28,11 @@
 
   ;; minify yes/no prompts
   (if (>= (string-to-number emacs-version) 28)
-      (setq use-short-answers t)
+      (defvar use-short-answers t)
     (defalias 'yes-or-no-p 'y-or-n-p))
 
   ;; do the things
-  (setq server-after-make-frame-hook 'mitch/graphical-setup)
+  (add-hook 'server-after-make-frame-hook #'mitch/graphical-setup)
   (if (display-graphic-p) (mitch/graphical-setup))
 
   ;; Control backups/swapfiles
@@ -61,13 +63,13 @@
 
   ;; straight.el: the better package manager?
   ;; minified bootstrap (split lines if you want) (or not)
-  (defvar bootstrap-version) (let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory)) (bootstrap-version 5)) (unless (file-exists-p bootstrap-file) (with-current-buffer (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el" 'silent 'inhibit-cookies) (goto-char (point-max)) (eval-print-last-sexp))) (load bootstrap-file nil 'nomessage)) (straight-use-package 'use-package) (setq straight-use-package-by-default t)
+  (defvar bootstrap-version) (let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory)) (bootstrap-version 5)) (unless (file-exists-p bootstrap-file) (with-current-buffer (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el" 'silent 'inhibit-cookies) (goto-char (point-max)) (eval-print-last-sexp))) (load bootstrap-file nil 'nomessage)) (straight-use-package 'use-package) (defvar straight-use-package-by-default t)
 
   (require 'mitch-packages)
 
   ;; Absolute line numbers. Relative ones are an annoyance to set up, sadly.
   (global-display-line-numbers-mode)
-  (setq display-line-numbers-width-start t)
+  (defvar display-line-numbers-width-start t)
 
   ;; scroll step stuff
   (setq scroll-margin 2
@@ -79,14 +81,8 @@
   ;; https://www.reddit.com/r/unixporn/comments/s7p7pr/so_which_run_launcher_do_you_use_rofi_or_dmenu/
   ;; I don't have it here because I don't use it right now.
 
-  ;; UTF-8 supremacy (Snippet from https://www.masteringemacs.org/article/working-coding-systems-unicode-emacs)
-  (prefer-coding-system 'utf-8)
-  (set-default-coding-systems 'utf-8)
-  (set-terminal-coding-system 'utf-8)
-  (set-keyboard-coding-system 'utf-8)
-  (if (boundp 'buffer-file-coding-system)
-      (setq-default buffer-file-coding-system 'utf-8)
-    (setq default-buffer-file-coding-system 'utf-8))
+  ;; UTF-8 supremacy (Snippet from https://github.com/doomemacs/doomemacs/blob/master/early-init.el)
+  (set-language-environment "UTF-8")
   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
   ;; Toggle Japanese with `qq'
@@ -101,8 +97,8 @@
 
   ;; Visualize whitespace. In a very chill and invisible way.
   (global-whitespace-mode t)
-  (setq whitespace-style '(face lines-tail))
-  (setq whitespace-line-column 80)
+  (defvar whitespace-style '(face lines-tail))
+  (defvar whitespace-line-column 80)
 
   ;; clean up modeline things
   (diminish 'lisp-interaction-mode)
@@ -113,9 +109,11 @@
 
   ;; save place in all files
   (save-place-mode t)
-  (setq save-place-file
-	(expand-file-name "file-position-save "backup-directory))
+  (defvar save-place-file
+	(expand-file-name "file-position-save" backup-directory))
 
   ;; load eshell stuff when we start eshell
   (add-hook 'eshell-mode-hook #'(lambda () (require 'eshell-settings)))
   )
+
+;;; init.el ends here

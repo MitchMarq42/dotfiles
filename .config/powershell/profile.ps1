388f140003679503f45d4dafe5c7bca264083f68
MitchMarq42 <dr.m.perseos@gmail.com>
2022-06-02 07:13:31 -0800

    powershell: give up on nice things

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -68,94 +68,94 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 # parens, and braces a nicer experience.  I'd like to include functions
 # in the module that do this, but this implementation still isn't as smart
 # as ReSharper, so I'm just providing it as a sample.
-Set-PSReadLineKeyHandler -Chord "'",'"' `
-  -BriefDescription SmartInsertQuote `
-  -LongDescription "Insert paired quotes if not already on a quote" `
-  -ScriptBlock {
-      param($key, $arg)
-      $line = $null
-      $cursor = $null
-      [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-      if ($line[$cursor] -eq $key.KeyChar) {
-	  # Just move the cursor
-	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-      }
-      else {
-	  # Insert matching quotes, move cursor to be in between the quotes
-	  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
-	  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
-      }
-  }
+# Set-PSReadLineKeyHandler -Chord "'",'"' `
+#   -BriefDescription SmartInsertQuote `
+#   -LongDescription "Insert paired quotes if not already on a quote" `
+#   -ScriptBlock {
+#       param($key, $arg)
+#       $line = $null
+#       $cursor = $null
+#       [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+#       if ($line[$cursor] -eq $key.KeyChar) {
+# 	  # Just move the cursor
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+#       }
+#       else {
+# 	  # Insert matching quotes, move cursor to be in between the quotes
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
+#       }
+#   }
 
-Set-PSReadLineKeyHandler -Key '(','{','[' `
-  -BriefDescription InsertPairedBraces `
-  -LongDescription "Insert matching braces" `
-  -ScriptBlock {
-      param($key, $arg)
-      $closeChar = switch ($key.KeyChar) {
-	  <#case#> '(' { [char]')'; break }
-	  <#case#> '{' { [char]'}'; break }
-	  <#case#> '[' { [char]']'; break }
-      }
-      $selectionStart = $null
-      $selectionLength = $null
-      [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-      $line = $null
-      $cursor = $null
-      [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-      if ($selectionStart -ne -1) {
-	  # Text is selected, wrap it in brackets
-	  [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $key.KeyChar + $line.SubString($selectionStart, $selectionLength) + $closeChar)
-	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-      } else {
-	  # No text is selected, insert a pair
-	  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)$closeChar")
-	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-      }
-  }
+# Set-PSReadLineKeyHandler -Key '(','{','[' `
+#   -BriefDescription InsertPairedBraces `
+#   -LongDescription "Insert matching braces" `
+#   -ScriptBlock {
+#       param($key, $arg)
+#       $closeChar = switch ($key.KeyChar) {
+# 	  <#case#> '(' { [char]')'; break }
+# 	  <#case#> '{' { [char]'}'; break }
+# 	  <#case#> '[' { [char]']'; break }
+#       }
+#       $selectionStart = $null
+#       $selectionLength = $null
+#       [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+#       $line = $null
+#       $cursor = $null
+#       [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+#       if ($selectionStart -ne -1) {
+# 	  # Text is selected, wrap it in brackets
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $key.KeyChar + $line.SubString($selectionStart, $selectionLength) + $closeChar)
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+#       } else {
+# 	  # No text is selected, insert a pair
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)$closeChar")
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+#       }
+#   }
 
-Set-PSReadLineKeyHandler -Key ')',']','}' `
-  -BriefDescription SmartCloseBraces `
-  -LongDescription "Insert closing brace or skip" `
-  -ScriptBlock {
-      param($key, $arg)
-      $line = $null
-      $cursor = $null
-      [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-      if ($line[$cursor] -eq $key.KeyChar) {
-	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-      } else {
-	  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
-      }
-  }
+# Set-PSReadLineKeyHandler -Key ')',']','}' `
+#   -BriefDescription SmartCloseBraces `
+#   -LongDescription "Insert closing brace or skip" `
+#   -ScriptBlock {
+#       param($key, $arg)
+#       $line = $null
+#       $cursor = $null
+#       [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+#       if ($line[$cursor] -eq $key.KeyChar) {
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+#       } else {
+# 	  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
+#       }
+#   }
 
-Set-PSReadLineKeyHandler -Key Backspace `
-  -BriefDescription SmartBackspace `
-  -LongDescription "Delete previous character or matching quotes/parens/braces" `
-  -ScriptBlock {
-      param($key, $arg)
-      $line = $null
-      $cursor = $null
-      [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-      if ($cursor -gt 0) {
-	  $toMatch = $null
-	  if ($cursor -lt $line.Length) {
-	      switch ($line[$cursor]) {
-		  <#case#> '"' { $toMatch = '"'; break }
-		  <#case#> "'" { $toMatch = "'"; break }
-		  <#case#> ')' { $toMatch = '('; break }
-		  <#case#> ']' { $toMatch = '['; break }
-		  <#case#> '}' { $toMatch = '{'; break }
-	      }
-	  }
-	  if ($toMatch -ne $null -and $line[$cursor-1] -eq $toMatch) {
-	      [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor - 1, 2)
-	  } else {
-	      [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteChar($key, $arg)
-	  }
-      }
-  }
+# Set-PSReadLineKeyHandler -Key Backspace `
+#   -BriefDescription SmartBackspace `
+#   -LongDescription "Delete previous character or matching quotes/parens/braces" `
+#   -ScriptBlock {
+#       param($key, $arg)
+#       $line = $null
+#       $cursor = $null
+#       [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+#       if ($cursor -gt 0) {
+# 	  $toMatch = $null
+# 	  if ($cursor -lt $line.Length) {
+# 	      switch ($line[$cursor]) {
+# 		  <#case#> '"' { $toMatch = '"'; break }
+# 		  <#case#> "'" { $toMatch = "'"; break }
+# 		  <#case#> ')' { $toMatch = '('; break }
+# 		  <#case#> ']' { $toMatch = '['; break }
+# 		  <#case#> '}' { $toMatch = '{'; break }
+# 	      }
+# 	  }
+# 	  if ($toMatch -ne $null -and $line[$cursor-1] -eq $toMatch) {
+# 	      [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor - 1, 2)
+# 	  } else {
+# 	      [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteChar($key, $arg)
+# 	  }
+#       }
+#   }
 
 # endregion Smart Insert/Delete
 
@@ -173,7 +173,7 @@ Set-PSReadLineKeyHandler -Key Backspace `
 #   }
 
 # aliases
-Set-Alias -Name 'ls' -Value 'Get-ChildItem'
+Set-Alias  -Name  'ls'  -Value  'Get-ChildItem'
 Set-Alias -Name 'which' -Value 'Get-Command'
 function rm(){ #defined as function instead of alias because parameters
     param([string[]]$allargs, [switch]$rf)
@@ -183,8 +183,8 @@ function rm(){ #defined as function instead of alias because parameters
 # use-module bootstrap
 if (! (Test-Path $home/.local/git/use-module/use-module.ps1)) {
     git clone https://git.mitchmarq42.xyz/mitch/use-module `
-      $home/.local/git/use-module}
-. ~/.local/git/use-module/use-module.ps1
+      $env:HOME/.local/git/use-module}
+. $env:HOME/.local/git/use-module/use-module.ps1
 
 # posh-git
 use-module 'posh-git' `

c8d43c69a936da9bbebdf523b61b88cca661a8ea
MitchMarq42 <dr.m.perseos@gmail.com>
2022-06-03 13:08:55 -0800

    setup script: indent comments, prepare for arch

--- .local/bin/mitch-setup.sh
+++ .local/bin/mitch-setup.sh
@@ -3,9 +3,12 @@
 # # On Ubuntu do:
 # sudo apt install git curl emacs zsh
 
+# Relevant directories
+mkdir -p ~/.local/bin ~/.local/git/
+
 # Get the dots function itself
 [ -f ~/.local/bin/dots ] ||
-    curl https://raw.githubusercontent.com/MitchMarq42/dotfiles/main/.local/bin/dots > ~/.local/bin/dots && chmod +x ~/.local/bin/dots
+	curl https://git.mitchmarq42.xyz/mitch/dotfiles/raw/branch/main/.local/bin/mitch-setup.sh > ~/.local/bin/dots && chmod +x ~/.local/bin/dots
 
 [ -d ~/.local/git/dotfiles ] ||
     git clone https://github.com/mitchmarq42/dotfiles --bare ~/.local/git/dotfiles
@@ -27,25 +30,28 @@ done
 dots submodule init
 dots submodule update
 
-# # On fedora do:
-# sudo dnf upgrade -y
-# sudo dnf install util-linux zsh emacs qutebrowser gnome-tweak-tool neovim gnome-extensions-app util-linux-user
-# sudo dnf install dnf-plugins-core
-# # rmpfusion
-# sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# On fedora do: {
+#     # Normal stuff
+#     sudo dnf upgrade -y
+#     sudo dnf install util-linux zsh emacs qutebrowser gnome-tweak-tool neovim gnome-extensions-app util-linux-user
+#     sudo dnf install dnf-plugins-core
+#     # rmpfusion
+#     sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#     # brave:
+#     sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+#     sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+#     sudo dnf install brave-browser
+#     # powershell:
+#     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+#     curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+#     sudo dnf check-update
+#     sudo dnf install compat-openssl10
+#     sudo dnf install -y powershell
+# }
 
-# # brave:
-# sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-# sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-# sudo dnf install brave-browser
-
-# # powershell:
-# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-# curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
-# sudo dnf check-update
-# sudo dnf install compat-openssl10
-# sudo dnf install -y powershell
-
+# On Arch do: {
+#     # arch things
+# }
 
 # # On macos do:
 # # check out
